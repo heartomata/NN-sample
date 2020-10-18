0c2c2bd1c2bd384230d6ca49263237dd60e1c7c2
heartomata <70837307+heartomata@users.noreply.github.com>
2020-10-18 22:47:29 +0800

    show top 5 error

---
+++ googlenet/naive.py
@@ -0,0 +1,240 @@
+
+import argparse
+import torch
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+
+NUM_CLASSES = 100
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
+class Inception(nn.Module):
+    def __init__(self, in_channel, n1_1, n3x3red, n3x3, n5x5red, n5x5, pool_plane):
+        super(Inception, self).__init__()
+        # first line
+        self.branch1x1 = BasicConv2d(in_channel, n1_1, kernel_size=1)
+
+        # second line
+        self.branch3x3 = nn.Sequential(
+            #BasicConv2d(in_channel, n3x3red, kernel_size=1),
+            BasicConv2d(in_channel, n3x3, kernel_size=3, padding=1, stride=1)
+        )
+
+        # third line
+        self.branch5x5 = nn.Sequential(
+            #BasicConv2d(in_channel, n5x5red, kernel_size=1),
+            BasicConv2d(in_channel, n5x5, kernel_size=5, padding=2, stride=1)
+        )
+
+        # fourth line
+        self.branch_pool = nn.Sequential(
+            nn.MaxPool2d(3, stride=1, padding=1),
+            #BasicConv2d(in_channel, pool_plane, kernel_size=1)
+        )
+
+    def forward(self, x):
+        y1 = self.branch1x1(x)
+        y2 = self.branch3x3(x)
+        y3 = self.branch5x5(x)
+        y4 = self.branch_pool(x)
+        output = torch.cat([y1, y2, y3, y4], 1)
+        return output
+
+
+class GoogleNet(nn.Module):
+    def __init__(self, num_classes=NUM_CLASSES):
+        super(GoogleNet, self).__init__()
+
+        self.conv1 = BasicConv2d(3, 64, kernel_size=7, stride=2, padding=3)
+
+        self.max_pool1 = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.conv_ex = BasicConv2d(64, 64, kernel_size=1) ## extra
+        self.conv2 = BasicConv2d(64, 192, kernel_size=3, stride=1, padding=1)
+
+        self.max_pool2 = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a3 = Inception(192, 64, 96, 128, 16, 32, 32) #192
+        self.b3 = Inception(416, 128, 128, 192, 32, 96, 64) #416 - 64
+
+        self.max_pool3 = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a4 = Inception(832, 192, 96, 208, 16, 48, 64)
+        self.b4 = Inception(1280, 160, 112, 224, 24, 64, 64)
+        self.c4 = Inception(1728, 128, 128, 256, 24, 64, 64)
+        self.d4 = Inception(2176, 112, 144, 288, 32, 64, 64)
+        self.e4 = Inception(2640, 256, 160, 320, 32, 128, 128)
+
+        self.max_pool4 = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a5 = Inception(3344, 256, 160, 320, 32, 128, 128)
+        self.b5 = Inception(4048, 384, 192, 384, 48, 128, 128)
+
+        self.avg_pool = nn.AvgPool2d(kernel_size=7, stride=1)
+
+        self.dropout = nn.Dropout(0.4)
+
+        self.classifier = nn.Linear(4944, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.max_pool1(x)
+        x = self.conv_ex(x) ## extra
+        x = self.conv2(x)
+        x = self.max_pool2(x)
+        x = self.a3(x)
+        x = self.b3(x)
+        x = self.max_pool3(x)
+        x = self.a4(x)
+        x = self.b4(x)
+        x = self.c4(x)
+        x = self.d4(x)
+        x = self.e4(x)
+        
+        x = self.max_pool4(x)
+        
+        x = self.a5(x)
+        x = self.b5(x)
+        
+        x = self.avg_pool(x)
+        x = self.dropout(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = nn.CrossEntropyLoss()(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    correct_5 = 0.0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            
+            #test_loss += nn.CrossEntropyLoss()(output, target, reduction='sum').item()  # sum up batch loss
+            _, pred = output.topk(5, 1, largest=True, sorted=True)  # get the index of the max log-probability
+            target = target.view(target.size(0), - 1).expand_as(pred)
+            correct = pred.eq(target).float()
+            correct_5 += correct[:, :5].sum()
+
+    #test_loss /= len(test_loader.dataset)
+
+    '''print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))'''
+    print("Top 5 err: ", 1 - correct_5.item() / len(test_loader.dataset))
+    print("Parameter numbers: {}".format(sum(p.numel() for p in model.parameters())))
+
+def data_tf(x):
+    x = x.resize((224,224), 2)
+    x = np.array(x, dtype='float32') / 255
+    x = (x - 0.5) / 0.5
+    x = x.transpose((2,0,1))
+    x = torch.from_numpy(x)
+
+    return x
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=9, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'batch_size': args.batch_size}
+    if use_cuda:
+        kwargs.update({'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True},
+                     )
+
+    transform = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        #transforms.Normalize(mean, std)
+    ])
+    dataset1 = datasets.CIFAR100('../data', train=True, download=True,
+                       transform=data_tf)
+    dataset2 = datasets.CIFAR100('../data', train=False,
+                       transform=data_tf)
+    train_loader = torch.utils.data.DataLoader(dataset1,**kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
+
+    model = GoogleNet().to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    print("Parameter numbers: {}".format(sum(p.numel() for p in model.parameters())))
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+if __name__ == '__main__':
+    main()
